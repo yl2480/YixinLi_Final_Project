28a449612eb5e89ca547e9c7fa192a2c84451879
yl2480 <36003439+yl2480@users.noreply.github.com>
2018-03-08 20:14:32 -0500

    final_8

--- js/clickcount.js
+++ js/clickcount.js
@@ -1,3 +1,14 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+var count = 0;
+
 $('.like').click(function() {
   count = count + 1;
   $('.currentCount').text(count)
